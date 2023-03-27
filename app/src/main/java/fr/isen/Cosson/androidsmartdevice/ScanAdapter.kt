f9ba47635bd5dd81c9799bbca7838e617fed1a25
LoaCos83 <67106370+LoaCos83@users.noreply.github.com>
2023-03-27 08:57:03 +0200

    initiat project

--- app/src/main/java/fr/isen/Cosson/androidsmartdevice/ScanAdapter.kt
+++ app/src/main/java/fr/isen/Cosson/androidsmartdevice/ScanAdapter.kt
@@ -5,11 +5,12 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import android.view.ViewParent
 import fr.isen.Cosson.androidsmartdevice.databinding.ActivityScanBinding
+import fr.isen.Cosson.androidsmartdevice.databinding.ScanCellBinding
 import java.text.FieldPosition
 import java.util.Scanner
 
-class ScanAdapter (var devices: ArrayList<String>) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+class ScanAdapter (var devices: ArrayList<String>) : RecyclerView.Adapter<ScanAdapter.ScanViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ScanViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = ScanCellBinding.inflate(inflater, parent, false)
         return ScanViewHolder(binding)
@@ -18,7 +19,7 @@ class ScanAdapter (var devices: ArrayList<String>) : RecyclerView.Adapter<Recycl
     override fun getItemCount(): Int = devices.size
 
     override fun onBindViewHolder(holder: ScanViewHolder, position: Int) {
-        TODO("holder.deviceName.text= devices(position)")
+        holder.deviceName.text = devices{position}
     }
     class ScanViewHolder(binding: ActivityScanBinding): RecyclerView.ViewHolder(binding.root){
         val deviceName = binding.deviceName
--- app/src/main/java/fr/isen/Cosson/androidsmartdevice/Scan_cell.kt
+++
@@ -1,4 +0,0 @@
-package fr.isen.Cosson.androidsmartdevice
-
-class Scan_cell {
-}
